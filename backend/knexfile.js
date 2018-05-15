d4c127af1c06c9b52d833fb3797fbf875b7a9315
Nyssa Keller <nyssa1@gmail.com>
2018-05-15 16:46:19 -0600

    Update knexfile to point to corrent database and use pg for client. Create inital migration

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,42 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: 'postgres://localhost/movie',
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './db/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds:  {
+      directory: './db/seeds/dev'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   }
-
 };
