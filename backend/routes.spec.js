332207025cd5143bd52a67723e699cd1034b1ffe
Nyssa Keller <nyssa1@gmail.com>
2018-05-15 16:57:25 -0600

    Set up backend test file

---
+++ backend/routes.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('API Routes', () => {
+  
+});
