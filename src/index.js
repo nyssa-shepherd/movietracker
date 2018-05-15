17afbf8cc7b189310fb74793c338e19520476aa8
Nyssa Keller <nyssa1@gmail.com>
2018-05-15 17:38:47 -0600

    Setup redux folders and create root reducer function in reducers/index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}></Provider>,
+  <Provider store={store}>{router}</Provider>,
   document.getElementById('root')
 );
 
