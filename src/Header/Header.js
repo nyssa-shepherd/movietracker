47c1adbf86a9af2129c8b41dacb18d0f81b817e7
Nyssa Keller <nyssa1@gmail.com>
2018-05-16 19:26:54 -0600

    Conditionally change login/logout link based on user state

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import './Header.css';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
   render() {
+    let log;
+    !this.state.user ? log = 'login' : log = 'logout';
+
     return (
       <div className='header'>
         <h1>Movie Tracker</h1>
+        <div className='nav-links'>
+          <NavLink to={log}>{log}</NavLink>
+        </div>
       </div>
     );
   };
