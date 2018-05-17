d697fea75923af48c3f9a8d9458dd926e3fab6ae
Nyssa Keller <nyssa1@gmail.com>
2018-05-16 19:46:10 -0600

    Change username state and password onChange of the inputs

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import './SignUp.css';
 
 class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      password: null
+    }
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }, () => {
+      console.log(this.state);
+    });
+  }
+
+  postUser = () => {
+
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={ () => this.postUser() }>
           <input type='text'
                  name='username'
-                 placeholder='username' />
+                 placeholder='username'
+                 onChange={ e => this.handleInputChange(e) } />
           <input type='password'
                  name='password'
-                 placeholder='password' />
+                 placeholder='password' 
+                 onChange={ e => this.handleInputChange(e) } />
           <button>Sign Up</button>
         </form>
       </div>
