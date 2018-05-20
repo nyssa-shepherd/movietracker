a6567e8dd01aa0f8b62537b5f8304a145fe745b9
Nyssa Keller <nyssa1@gmail.com>
2018-05-20 13:38:56 -0600

    Conditionally change if login or sign up form is seen

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import SignIn from '../SignIn/SignIn';
 import './SignUp.css';
 
 class SignUp extends Component {
@@ -8,7 +10,8 @@ class SignUp extends Component {
       username: null,
       password: null,
       existingUsers: null,
-      usernameExistsMessage: null
+      usernameExistsMessage: null,
+      upOrIn: 'Sign Up'
     }
   }
 
@@ -51,26 +54,59 @@ class SignUp extends Component {
     this.setState({ usernameExistsMessage: 'Error username already exists' });
   }
 
+  toggleUpOrIn = () => {
+    const { upOrIn } = this.state;
+    upOrIn === 'Sign In' ? this.setState({upOrIn: 'Sign Up'}) : this.setState({upOrIn: 'Sign In'});
+  }
+
   render() {
-    const { usernameExistsMessage } = this.state;
+    const { usernameExistsMessage, upOrIn } = this.state;
     const errorMessage = usernameExistsMessage ? <h5>{usernameExistsMessage}</h5> : null;
+    const signup = (        
+      <form id='sign-up'
+          onSubmit={ e => this.checkIfUsernameExists(e) }>
+      <h3 className='signup-text'>Sign Up</h3>
+      {errorMessage}
+      <input type='text'
+            name='username'
+            placeholder='Username'
+            onChange={ e => this.handleInputChange(e) } />
+      <input type='password'
+            name='password'
+            placeholder='Password' 
+            onChange={ e => this.handleInputChange(e) } />
+      <input type='password'
+            name='password'
+            placeholder='Confirm Password' 
+            onChange={ e => this.handleInputChange(e) } />
+      <button disabled={!this.state.username || !this.state.password}>Sign Up</button>
+      <button onClick={() => this.toggleUpOrIn()}>Sign In</button>
+      </form>
+    );
+
+    const signin = (
+      <form id='sign-up'
+            onSubmit={ e => this.getUser(e) }>
+        <h3 className='signup-text'>Sign In</h3>
+        {errorMessage}
+        <input type='text'
+              name='Username'
+              placeholder='username'
+              onChange={ e => this.handleInputChange(e) } />
+        <input type='password'
+              name='password'
+              placeholder='Password' 
+              onChange={ e => this.handleInputChange(e) } />
+        <button disabled={!this.state.username || !this.state.password}>Sign In</button>
+        <button onClick={() => this.toggleUpOrIn()}>Sign Up</button>
+      </form>
+    );
+
+    const correctForm = upOrIn === 'Sign Up' ? signup : signin;
 
     return (
       <div>
-        <form id='sign-up'
-              onSubmit={ e => this.checkIfUsernameExists(e) }>
-          <h3 className='signup-text'>Sign Up</h3>
-          {errorMessage}
-          <input type='text'
-                 name='username'
-                 placeholder='username'
-                 onChange={ e => this.handleInputChange(e) } />
-          <input type='password'
-                 name='password'
-                 placeholder='password' 
-                 onChange={ e => this.handleInputChange(e) } />
-          <button disabled={!this.state.username || !this.state.password}>Sign Up</button>
-        </form>
+        {correctForm}
       </div>
     );
   }
