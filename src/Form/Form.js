84b64daa1012fa4d3cad814544dc672ece773428
Nyssa Keller <nyssa1@gmail.com>
2018-05-20 13:42:02 -0600

    Create Form component

---
+++ src/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SignUp from '../SignUp/SignUp';
+import SignIn from '../SignIn';
+import './Form.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default Form;
