76ba2e1a6063308aeaff4c80cd6b767c640098ab
Nyssa Keller <nyssa1@gmail.com>
2018-05-20 14:06:38 -0600

    Update state in SignIn component on input change
    Find user that matches input username

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import './SignIn.css';
 
 class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      password: null
+    }
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }, () => {
+      console.log(this.state);
+    });
+  }
+
+  getUser = e => {
+    e.preventDefault();
+    const { existingUsers } = this.props;
+    const { username, password } = this.state;
+    const matchingUser = existingUsers.find(user => user.username === username);
+    console.log(matchingUser);
+  }
+
   render() {
     return (
       <div>
@@ -10,8 +33,8 @@ class SignIn extends Component {
         <h3 className='signin-text'>Sign In</h3>
         {/* {errorMessage} */}
         <input type='text'
-              name='Username'
-              placeholder='username'
+              name='username'
+              placeholder='Username'
               onChange={ e => this.handleInputChange(e) } />
         <input type='password'
               name='password'
