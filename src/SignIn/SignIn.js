f3feecbb3916184b302d4cf3fcec39374ba942e1
Nyssa Keller <nyssa1@gmail.com>
2018-05-20 13:43:23 -0600

    Create SignIn component

---
+++ src/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './SignIn.css';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default SignIn;
