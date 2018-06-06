a5043479892e6ef1cd07875df1fa6a4d807ce686
Nyssa Keller <nyssa1@gmail.com>
2018-06-06 11:06:02 -0600

    Create favorites component

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Favorites;
